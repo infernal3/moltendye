3f393ea8e7576e12acacba4f907de9a0a873dbbd
infernal3 <infernal1559@gmail.com>
2025-07-27 15:38:30 -0500

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -7,10 +7,11 @@ const cx = () => cv().getContext("2d");
 const wx = () => window.innerWidth;
 const wy = () => window.innerHeight;
 const data = {menu: 0};
-
+console.log("debug flag 0");
 const TitleScreen = function() {
     cx().reset();
     cx().font = "30px monospace";
+    console.log("debug flag 1");
     if(wx() < 640 || wy() < 640) {
         cx().fillText("Your screen is too small to play this!", 0, 0);
         cx().fillText(`Current dims: (${wx()}, ${wy()})`, 0, 40);
@@ -20,5 +21,6 @@ const TitleScreen = function() {
     cx().fillText("Start Game", 40, 240);
     cx().fillText("Options", 40, 300);
     cx().fillText("Quit Game", 40, 360);
+    console.log("debug flag 2");
 }
 window.addEventListener("load",TitleScreen,{passive: true});
