da5f0bb999f86911dabff90b1cd04da0f78c4ee7
infernal3 <infernal1559@gmail.com>
2025-07-27 13:38:04 -0500

    Create canvas.js

---
+++ js/canvas.js
@@ -0,0 +1,24 @@
+/**
+ * Original script by infernal3 - Moltendye
+ */
+const el = e => document.getElementById(e);
+const cv = () => el("app");
+const cx = () => cv().getContext("2d");
+const wx = () => window.innerWidth;
+const wy = () => window.innerHeight;
+const data = {menu: 0};
+
+const TitleScreen = function() {
+    cx().reset();
+    cx().font = "30px monospace";
+    if(wx() < 640 || wy() < 640) {
+        cx().fillText("Your screen is too small to play this!", 0, 0);
+        cx().fillText(`Current dims: (${wx()}, ${wy()})`, 0, 40);
+        cx().fillText(`Required: (640, 640) or more`, 0, 40);
+        return;
+    }
+    cx().fillText("Start Game", 40, 240);
+    cx().fillText("Options", 40, 300);
+    cx().fillText("Quit Game", 40, 360);
+}
+window.addEventListener("load",TitleScreen,{passive: true});
