04ff72c292d6eed596555486e0519a7458721922
infernal3 <infernal1559@gmail.com>
2025-08-02 17:27:15 -0500

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -1,11 +1,11 @@
 const A_DATA = [
-  {id: 1, name: "First steps", lore: "Experience the game over screen."},
-  {id: 2, name: "Survivor", lore: "Survive for 1 minute on any difficulty."},
-  {id: 3, name: "Optional", lore: "Change all options to be different from the defaults."},
-  {id: 4, name: "The Swarm", lore: "Have 100 bullets on screen at once."},
+  {id: 1, name: "First Steps", lore: "Experience the game over screen."},
+  {id: 2, name: "Survivalist", lore: "Survive for 1 minute on any difficulty."},
+  {id: 3, name: "Option Manic", lore: "Change all options to be different from the defaults."},
+  {id: 4, name: "Drone Swarm", lore: "Have 100 bullets on screen at once."},
   {id: 5, name: "Knife's Edge", lore: "Be on 1 health for 3 minutes without dying."},
-  {id: 6, name: "1.8 reference", lore: "Survive for 1.8 minutes on a difficulty of at least 1.8."},
-  {id: 7, name: "6 / 5", lore: "Survive for 6 seconds on a difficulty of at least 5."},
+  {id: 6, name: "1.8 players", lore: "Survive for 1.8 minutes on a difficulty of at least 1.8."},
+  {id: 7, name: "6 out of 5", lore: "Survive for 6 seconds on a difficulty of at least 5."},
   {id: 8, name: "True Expert", lore: "Survive for 10 minutes on any difficulty."},
-  {id: 9, name: "secret", lore: "Requires finding an Easter Egg!"}
+  {id: 9, name: "Small Number", lore: "Die within 0.250 seconds or less."}
 ]
